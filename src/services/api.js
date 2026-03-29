80a5032693a53e053ffe2bb1947c737d6615edb4
eZice <144651926+esyice@users.noreply.github.com>
2026-03-29 09:37:23 +0530

    Update api.js with new API configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/admin',
+  baseURL: 'https://martapi.anshvarma.in/api/admin',
 });
 
 api.interceptors.request.use((config) => {
